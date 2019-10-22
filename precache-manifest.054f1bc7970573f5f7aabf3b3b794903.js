964fb18066d90dbf45b6cd72daf330de755580a5
anx <anx@ulzq.de>
2019-10-22 09:48:51 +0200

    Initialize gh-pages

---
+++ precache-manifest.054f1bc7970573f5f7aabf3b3b794903.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4717bccbb31a82172b527a8da9d484da",
+    "url": "/dci_cannon_fudder/index.html"
+  },
+  {
+    "revision": "b12f7396c37f2404bfa8",
+    "url": "/dci_cannon_fudder/static/css/main.384dd5fa.chunk.css"
+  },
+  {
+    "revision": "b592801a8db1b358e532",
+    "url": "/dci_cannon_fudder/static/js/2.d32fbb06.chunk.js"
+  },
+  {
+    "revision": "b12f7396c37f2404bfa8",
+    "url": "/dci_cannon_fudder/static/js/main.4899932b.chunk.js"
+  },
+  {
+    "revision": "6ba36daf40df3415c181",
+    "url": "/dci_cannon_fudder/static/js/runtime-main.3b4f3a91.js"
+  },
+  {
+    "revision": "f5d6e48befd3d5880b1b1d24c9b26be7",
+    "url": "/dci_cannon_fudder/static/media/logo.f5d6e48b.svg"
+  }
+]);
